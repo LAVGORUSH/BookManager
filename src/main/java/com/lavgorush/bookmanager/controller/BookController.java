b3e4fa0cc7c98ceda0fbe0c6009c99a3b13a5422
lavgorush <lavgorush@gmail.com>
2018-04-26 14:59:58 +0300

    Add Book Controller

--- src/main/java/com/lavgorush/bookmanager/controller/BookController.java
+++ src/main/java/com/lavgorush/bookmanager/controller/BookController.java
@@ -1,12 +1,19 @@
 package com.lavgorush.bookmanager.controller;
 
+import com.lavgorush.bookmanager.model.Book;
 import com.lavgorush.bookmanager.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BookController {
+
     private BookService bookService;
 
     @Autowired
@@ -14,4 +21,44 @@ public class BookController {
     public void setBookService(BookService bookService) {
         this.bookService = bookService;
     }
+
+    @GetMapping(value = "books")
+    public String listBooks(Model model) {
+        model.addAttribute("book", new Book());
+        model.addAttribute("listBooks", this.bookService.listBook());
+
+        return "books";
+    }
+
+    @PostMapping(value = "/books/add")
+    public String addBook(@ModelAttribute("book") Book book) {
+        if (book.getId() == 0) {
+            this.bookService.addBook(book);
+        } else {
+            this.bookService.updateBook(book);
+        }
+
+        return "redirect:/books";
+    }
+
+    @GetMapping("remove/${id}")
+    public String removeBook(@PathVariable("id") int id) {
+        this.bookService.removeBook(id);
+        return "redirect:/books";
+    }
+
+    @GetMapping("edit/${id}")
+    public String editBook(@PathVariable("id") int id, Model model) {
+        model.addAttribute("book", this.bookService.getBookById(id));
+        model.addAttribute("listBooks", this.bookService.listBook());
+        return "books";
+    }
+
+    @GetMapping("bookdata/${id}")
+    public String bookData(@PathVariable("id") int id, Model model) {
+        model.addAttribute("book", this.bookService.getBookById(id));
+
+        return "bookdata";
+    }
+
 }
